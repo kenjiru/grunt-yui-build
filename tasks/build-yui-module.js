48b3b6b8ba9ccab4e0ddfaa59d615d535aeff836
kenjiru <kenjiru.ro@gmail.com>
2014-02-28 12:13:42 +0100

    Call the 'done' callback when something fails. Added some checks.

--- tasks/build-yui-module.js
+++ tasks/build-yui-module.js
@@ -9,11 +9,16 @@
 'use strict';
 
 var walk = require('../lib/walk.js'),
+    watch = require('../lib/watch.js'),
     buildYuiModule = require('../lib/build-yui-module.js');
 
 module.exports = function (grunt) {
     grunt.registerTask('walk', 'Scan for modules.', function() {
         walk.init.call(this, grunt);
+    });
+
+    grunt.registerTask('watch', 'Wais for modules to change and then builds them.', function() {
+        watch.init.call(this, grunt);
     });
 
     grunt.registerTask('build-yui-modules', 'Builds a single YUI3 module.', function() {
